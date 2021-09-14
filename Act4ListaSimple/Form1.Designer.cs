c81b0fe7263e735fac21a90cdff7f5fa502c4aca
RaquelMMtz <70656439+RaquelMMtz@users.noreply.github.com>
2021-09-13 19:52:23 -0500

    Add files via upload

---
+++ Act4ListaSimple/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Act4ListaSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsbLista = new System.Windows.Forms.ListBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.LblDato = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lsbLista
+            // 
+            this.lsbLista.FormattingEnabled = true;
+            this.lsbLista.Location = new System.Drawing.Point(312, 30);
+            this.lsbLista.Name = "lsbLista";
+            this.lsbLista.Size = new System.Drawing.Size(144, 147);
+            this.lsbLista.TabIndex = 22;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(38, 104);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefono.TabIndex = 21;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(95, 104);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 20;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(95, 74);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 19;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(43, 77);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 18;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(213, 132);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 17;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(213, 102);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 16;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(213, 73);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(213, 44);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 14;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(95, 46);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 13;
+            // 
+            // LblDato
+            // 
+            this.LblDato.AutoSize = true;
+            this.LblDato.Location = new System.Drawing.Point(43, 49);
+            this.LblDato.Name = "LblDato";
+            this.LblDato.Size = new System.Drawing.Size(44, 13);
+            this.LblDato.TabIndex = 12;
+            this.LblDato.Text = "Numero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 221);
+            this.Controls.Add(this.lsbLista);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.LblDato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbLista;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label LblDato;
+    }
+}
+
---
+++ Act4ListaSimple/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Act4ListaSimple
+{
+    public partial class Form1 : Form
+    {
+        Lista miLista;
+        Graphics graficos;
+        public Form1()
+        {
+            InitializeComponent();
+            miLista = new Lista();
+            this.WindowState = FormWindowState.Maximized;
+            graficos = this.CreateGraphics();
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int d = int.Parse(txtNumero.Text);
+            string nombre = txtNombre.Text;
+            string telefono = txtTelefono.Text;
+            Nodo n = new Nodo(d, nombre, telefono);
+            miLista.Agregar(n);
+            txtNumero.Clear();
+            txtNumero.Focus();
+            txtNombre.Clear();
+            txtTelefono.Clear();
+            miLista.Mostrar(lsbLista);
+            //MessageBox.Show(miLista + "");
+            for (int i = 0; i < lsbLista.Items.Count; i++)
+            {
+                DibujarRectangulo(i);
+            }
+        }
+        public void DibujarRectangulo (int x)
+        {
+            Pen pluma = new Pen(Color.Black, 3);
+            Rectangle rectangulo = new Rectangle(10+(x*120), 400, 100, 50);
+            graficos.DrawRectangle(pluma, rectangulo);
+            graficos.DrawLine(pluma, 80 + (x*120), 400, 80 + (x * 120), 450);
+            graficos.DrawLine(pluma, 95 + (x * 120), 425, 130 + (x * 120), 425);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int d = int.Parse(txtNumero.Text);
+                miLista.Eliminar(d);
+                txtNumero.Clear();
+                txtNombre.Clear();
+                txtTelefono.Clear();
+                txtNumero.Focus();
+                miLista.Mostrar(lsbLista);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+
+            //MessageBox.Show(miLista + "");
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int d = int.Parse(txtNumero.Text);
+            Nodo b = new Nodo();
+            if (miLista.Buscar(d, ref b))
+            {
+                txtNombre.Text = b.Nombre;
+                txtTelefono.Text = b.Telefono;
+                //MessageBox.Show("Existe ");
+            }
+            else
+            {
+                txtNombre.Clear();
+                txtTelefono.Clear();
+                MessageBox.Show("NO Existe");
+            }
+            txtNumero.Focus();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(txtNumero.Text);
+                miLista.Modificar(numero, txtNombre.Text, txtTelefono.Text);
+                txtNumero.Clear();
+                txtNombre.Clear();
+                txtTelefono.Clear();
+                txtNumero.Focus();
+                miLista.Mostrar(lsbLista);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+        }
+    }
+}
